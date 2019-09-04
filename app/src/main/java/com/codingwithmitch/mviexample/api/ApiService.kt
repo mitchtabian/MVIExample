7d11f55e29e16214d9c61e2881a3571146dd4381
Mitch Tabian <mitch@tabian.ca>
2019-09-04 09:30:04 -0700

    Add GenericApiResponse wrapper

--- app/src/main/java/com/codingwithmitch/mviexample/api/ApiService.kt
+++ app/src/main/java/com/codingwithmitch/mviexample/api/ApiService.kt
@@ -3,16 +3,17 @@ package com.codingwithmitch.mviexample.api
 import androidx.lifecycle.LiveData
 import com.codingwithmitch.mviexample.model.BlogPost
 import com.codingwithmitch.mviexample.model.User
+import com.codingwithmitch.mviexample.util.GenericApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("placeholder/blogs")
-    fun getBlogPosts(): LiveData<List<BlogPost>>
+    fun getBlogPosts(): LiveData<GenericApiResponse<List<BlogPost>>>
 
     @GET("placeholder/user/{userId}")
     fun getUser(
         @Path("userId") userId: String
-    ): LiveData<User>
+    ): LiveData<GenericApiResponse<User>>
 }
